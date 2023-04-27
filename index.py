9b122ef8e374b718e1cf91b8f6f71e236cf63415
“YuQiao0428” <leah.huang0428@gmail.com>
2023-04-27 16:28:34 +0800

    make it better

--- index.py
+++ index.py
@@ -1,9 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
-
+import openai
+import os
+openai.api_key = os.getenv("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")
 @app.route("/")
 def index():
-    return "測試網頁"
-
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="靜宜大學資管系教學特色？",
+        max_tokens=128,
+        temperature=0.5,
+    )
+    msg = response.choices[0].text
+    return msg
 if __name__ == "__main__":
     app.run()
